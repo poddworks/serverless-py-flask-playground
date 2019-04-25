d63442c958a85e569ecc97c3d084b6269d3adf88
Jeffrey Jen <yihungjen@gmail.com>
2019-04-25 17:45:30 +0800

    patch inverted cache db connect setup

--- cache.py
+++ cache.py
@@ -4,6 +4,6 @@ import environment
 
 rcli = None
 if os.environ.get('DEPLOY_STATUS', 'development') == 'development':
-  rcli = redis.Redis(host=environment.REDIS_URI, port=environment.REDIS_PORT, db=environment.RESIS_DB_NAME)
-else:
   rcli = redis.Redis(host='localhost', port=32768, db=0)
+else:
+  rcli = redis.Redis(host=environment.REDIS_URI, port=environment.REDIS_PORT, db=environment.RESIS_DB_NAME)
